7a45adfb62be967c20e3b972e215437754bad14c
Urfeen Jus <urfeen@gmail.com>
2021-01-29 19:49:51 +0300

    navbar logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter } from "react-router-dom";
+import Routers from "Routers.js";
+import TopBar from "components/TopBar/TopBar.js"
+import "./index.scss";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles"
+import { Paper } from "@material-ui/core"
+
+
+const App = () => {
+  const [isDarkTheme, SetDarkTheme] = useState(false)
+  const theme = createMuiTheme({
+    palette: {
+      type: isDarkTheme ? "dark" : "light",
+    },
+  })
+
+  const toggleThemeHandler = () => {
+    SetDarkTheme((prevTheme) => !prevTheme);
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Paper>
+        <BrowserRouter>
+          <TopBar toggleTheme={toggleThemeHandler} isDarkTheme={isDarkTheme} />
+          <Routers />
+
+        </BrowserRouter>
+      </Paper>
+    </ThemeProvider>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
