8eb0a0e9e4acabb5697789b29cf9a64424cc418c
Urfeen Jus <urfeen@gmail.com>
2021-01-29 22:25:33 +0300

    add some styles to navbar

--- src/index.js
+++ src/index.js
@@ -2,31 +2,52 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import Routers from "Routers.js";
-import TopBar from "components/TopBar/TopBar.js"
+import TopBar from "components/TopBar/TopBar.js";
 import "./index.scss";
-import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles"
-import { Paper } from "@material-ui/core"
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { Paper } from "@material-ui/core";
+import { lightBlue, indigo, deepPurple, pink } from "@material-ui/core/colors";
 
 
 const App = () => {
-  const [isDarkTheme, SetDarkTheme] = useState(false)
-  const theme = createMuiTheme({
+  const [isDarkTheme, SetDarkTheme] = useState(!!localStorage.getItem("isDarkTheme"))
+  const darkTheme = createMuiTheme({
     palette: {
-      type: isDarkTheme ? "dark" : "light",
+      type: "dark",
+      primary: {
+        main: lightBlue[900],
+      },
+      secondary: {
+        main: indigo[900],
+      },
+    },
+  })
+  const lightTheme = createMuiTheme({
+    palette: {
+      type: "light",
+    },
+    primary: {
+      main: deepPurple.A200,
+    },
+    secondary: {
+      main: pink[700],
     },
   })
 
   const toggleThemeHandler = () => {
-    SetDarkTheme((prevTheme) => !prevTheme);
+    SetDarkTheme((prevTheme) => {
+      localStorage.setItem("isDarkTheme", !prevTheme);
+      return (!prevTheme)
+    });
+
   }
 
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
       <Paper>
         <BrowserRouter>
           <TopBar toggleTheme={toggleThemeHandler} isDarkTheme={isDarkTheme} />
           <Routers />
-
         </BrowserRouter>
       </Paper>
     </ThemeProvider>
